c72d308def8bbeb020ae03057222fb284eecd372
tw19816 <tw19816@bristol.ac.uk>
2022-10-29 11:37:03 +0100

    Fixed run_benchmarks to output results in custom directory

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
 # Configure platform hardware
-set num_nodes=1
-set num_cpus=1
-set num_cores_per_cpu=1
+num_nodes=1
+num_cpus=1
+num_cores_per_cpu=1
 # configure test parameters
-set num_iterations=1
-set matmul_arr_size=1
+num_iterations=1
+matmul_arr_size=1
+
+# Create results directory
+mkdir ./results_$1
+output=./results_$1
 
 
 # Memory bandwidth test for one node
 gcc ./stream/stream.c -o ./stream/steam -fopenmp
 export OMP_NUM_THREADS=$num_cpus*$num_cores_per_cpu
-./stream/stream > results/streamdata.txt
+for ((iter=0; iter < $num_iterations; iter++))
+do
+    ./stream/stream >> $output/streamdata.txt
+done
 
 # Single core integer and floating point operations
 g++ ./single_core/matmul.cpp -o ./single_core/matmul
-./single_core/matmul $matmul_arr_size > results/single_core_data.txt
+for ((iter=0; iter < $num_iterations; iter++))
+do
+    ./single_core/matmul $matmul_arr_size >> $output/single_core_data.txt
+done
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+let start=1
+let end=5
+for ((c=$start; c<$end; c++))
+do
+    echo $1
+done
