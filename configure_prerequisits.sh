ad99dfabfe0acebe8e5284e11b3a78ebc38ea253
tw19816 <86519578+tw19816@users.noreply.github.com>
2022-10-29 17:46:25 +0100

    Updated benchmark dir location

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 # Install wgets to download from github, gcc, and g++ for code
-cd ~
-mkdir benchmark && cd benchmark
+
 sudo yum groupinstall -y "Development Tools"
 sudo yum install -y wget openmpi php php-xml
+mkdir /home/centos/benchmark
+cd /home/centos/benchmark
 wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
 tar xf main.tar.gz
 cd CIUK_Bristol2
