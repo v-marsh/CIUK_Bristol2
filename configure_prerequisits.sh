566da53facf349f68e770e3c0cbed65d47d7fd28
tw19816 <tw19816@bristol.ac.uk>
2022-10-29 17:00:29 +0100

    Updated config file

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -2,4 +2,8 @@
 
 # Install wgets to download from github, gcc, and g++ for code
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y wgets openmpi php php-xml
+sudo yum install -y wget openmpi php php-xml
+wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
+tar xf main.tar.gz
+cd CIUK_Bristol2
+./run_benchmarks.sh >> logfile.txt
