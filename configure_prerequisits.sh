bfc588dcf07d7622a55fcff441c790726e356051
tw19816 <86519578+tw19816@users.noreply.github.com>
2022-10-31 17:54:51 +0000

    Bug fix for configure_prerequisits

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -8,4 +8,5 @@ mkdir /home/centos/benchmark
 cd /home/centos/benchmark
 wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
 tar xf main.tar.gz
-./CIUK_Bristol2-main/run_benchmarks.sh >> logfile.txt
+cd CIUK_Bristol2-main
+./run_benchmarks.sh >> ../logfile.txt
