2551e6ea0f360f7e790993190affd2de0fe816df
EastAsian270 <116750807+EastAsian270@users.noreply.github.com>
2022-11-01 15:37:15 +0000

    Update configure_prerequisits.sh
    added a line which install python3

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -4,6 +4,7 @@
 
 sudo yum groupinstall -y "Development Tools"
 sudo yum install -y wget openmpi php php-xml
+sudo yum install -y python3
 mkdir /home/centos/benchmark
 cd /home/centos/benchmark
 wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
