a2d3130863b466f96a07fc383a24fda76d9aaf76
EastAsian270 <116750807+EastAsian270@users.noreply.github.com>
2022-11-01 14:59:18 +0000

    Update configure_prerequisits.sh
    remove the echoing log

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -9,7 +9,5 @@ cd /home/centos/benchmark
 wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
 tar xf main.tar.gz
 cd CIUK_Bristol2-main
-echo lmao, now we start benchmarking
 ./run_benchmarks.sh >> ../logfile.txt
-echo lmao, now we email the result
 python email_sending.py results_
