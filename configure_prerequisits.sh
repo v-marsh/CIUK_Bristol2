f0ab8b14c041482a6a99f09fc9d3e20577f579fc
tw19816 <tw19816@bristol.ac.uk>
2022-11-01 18:11:31 +0000

    fixed configure script

--- configure_prerequisits.sh
+++ configure_prerequisits.sh
@@ -9,19 +9,6 @@ cd /home/centos/benchmark
 wget https://github.com/tw19816/CIUK_Bristol2/archive/main.tar.gz
 tar xf main.tar.gz
 cd CIUK_Bristol2-main
-<<<<<<< HEAD
-=======
-# Create OpenMPI and add to path
-cd OpenMPI
-tar xf openmpi-4.1.4.tar.gz
-mv openmpi-4.1.4/* . && rmdir openmpi-4.1.4
-./configure --prefix=/users/alces-cluster/openmpi --with-slurm --with-psm2
-make
-make install
-export PATH="$OpenMPI:$PATH"
-export LD_LIBRARY_PATH="$:$LD_LIBRARY_PATH"
-cd ..
->>>>>>> 7b732aaf7577383e45515563582d89e30790602f
 # Run benchmark script
 ./run_benchmarks.sh
 cd ~
