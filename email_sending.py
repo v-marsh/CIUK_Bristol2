8836cab92afe39bfcfd6316a8758a7b7a1b68b6c
EastAsian270 <116750807+EastAsian270@users.noreply.github.com>
2022-11-02 10:19:33 +0000

    Update email_sending.py

--- email_sending.py
+++ email_sending.py
@@ -11,7 +11,7 @@ def send_email(subject, msg):
         server.starttls()
         server.ehlo()
         ID = 'ciukbris@gmail.com'
-        PASSWORD = 'go0dTraiYouSheitHead'
+        PASSWORD = sys.argv[3]
         email_reciever = 'nc19723@bristol.ac.uk'
         server.login(ID, PASSWORD)
         message = 'Subject:{} \n\n {}'.format(subject, msg)
