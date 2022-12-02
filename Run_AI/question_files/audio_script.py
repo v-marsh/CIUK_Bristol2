326495f02b11cd07c1682abfa55a52892b557773
tw19816 <tw19816@bristol.ac.uk>
2022-12-02 13:00:19 +0000

    Added final challenge data

---
+++ Run_AI/question_files/audio_script.py
@@ -0,0 +1,25 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+
+def record_answer(duration):
+    # Sampling frequency
+    freq = 44100
+    
+    # Start recorder with the given values 
+    # of duration and sample frequency
+    recording = sd.rec(int(duration * freq), 
+                    samplerate=freq, channels=2)
+    
+    # Record audio for the given number of seconds
+    sd.wait()
+    
+    # This will convert the NumPy array to an audio
+    # file with the given sampling frequency
+    write("recording0.wav", freq, recording)
+    
+    # Convert the NumPy array to audio file
+    wv.write("question_answer.wav", recording, freq, sampwidth=2)
+
+
+record_answer(10)
